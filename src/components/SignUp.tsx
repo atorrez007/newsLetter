0a9e0544d4cc17ba85dc6c978f318f7f0365833b
funkee <xxtrm98@protonmail.com>
2023-09-21 15:41:04 -0500

    prelim left sided styling

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import successIcon from "../assets/images/icon-success.svg";
 import "./SignUpStyles.css";
 const SignUp = () => {
-  return <div className="signup-box"></div>;
+  return (
+    <div className="signup-box">
+      <div className="left-sided-content">
+        <h1 className="left-sided-header">Stay updated!</h1>
+        <div className="left-sided-paragraph">
+          <p className="textRegular">
+            Join 60,000+ product managers receiving monthly updates on:
+          </p>
+        </div>
+        <div className="bullet-section">
+          <div className="bullet-points">
+            <img
+              src={successIcon}
+              alt="success icon"
+              className="icon-style"
+            ></img>
+            <p>Product discovery and building what matters</p>
+          </div>
+          <div className="bullet-points">
+            <img
+              src={successIcon}
+              alt="success icon"
+              className="icon-style"
+            ></img>
+            <p>Measuring to ensure updates are a success</p>
+          </div>
+          <div className="bullet-points">
+            <img
+              src={successIcon}
+              alt="success icon"
+              className="icon-style"
+            ></img>
+            <p>And much more!</p>
+          </div>
+        </div>
+      </div>
+      <div className="right-sided-content">
+        <h1 className="text">Right sided content</h1>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
