152c8a39c95bf4a156e5d5ff8e6be5d513d8281e
funkee <xxtrm98@protonmail.com>
2023-09-25 09:37:03 -0500

    added routing to site and corrected style warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import SignUp from "./components/SignUp";
 import SuccessMessage from "./components/SuccessMessage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="signup-container">
-      <SignUp />
-      {/* <SuccessMessage /> */}
+      <Routes>
+        <Route path="/" element={<SignUp />}></Route>
+        <Route path="/success" element={<SuccessMessage />}></Route>
+      </Routes>
     </div>
   );
 }
